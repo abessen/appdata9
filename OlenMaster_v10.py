0d2ff9496f4576960880c92f3c00340ee54e9a97
abessen <abessen@quarryvision.com>
2023-01-03 19:22:17 -0600

    autoupdate

---
+++ OlenMaster_v10.py
@@ -0,0 +1,16 @@
+from pathlib import Path  # Python Standard Library
+import pandas as pd  # pip install pandas openpyxl
+
+
+this_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
+wb_file_path = this_dir / 'OlenMaster_v10.xlsb'
+
+df = pd.read_excel(
+    io=wb_file_path,
+    engine='openpyxl',
+    sheet_name='eWon',
+    skiprows=18,
+    usecols='BL:BY',
+    nrows=1421,
+)
+print(df)
